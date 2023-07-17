2c892deaa6b528913461857d1ad1686bdabb9357
Kevin El-Khoury <incorrectclock1@gmail.com>
2023-07-17 09:17:00 -0400

    Course Completed

---
+++ 6 Binary Search Trees/lists-abv.rkt
@@ -0,0 +1,9 @@
+(cons "a" (cons "b" (cons "c" empty)))
+(list "a" "b" "c")
+(list (+ 1 2) (+ 3 4) (+ 5 6))
+
+(define L1 (list "b" "c"))
+(define L2 (list "d" "e" "f"))
+(cons "a" L1)
+
+(append L1 L2)
